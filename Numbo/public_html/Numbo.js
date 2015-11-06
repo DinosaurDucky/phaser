2588eac723102f0c37eceafe3f0550aab3c7c9dd
DinosaurDucky <bhatch@ucsc.edu>
2015-11-05 17:49:29 -0800

    numbers spawn & are clickable now :D

--- Numbo/public_html/Numbo.js
+++ Numbo/public_html/Numbo.js
@@ -1,129 +1,88 @@
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '',
         {preload: preload, create: create, update: update});
-
 var platforms;
-var player;
 var cursors;
-var stars;
-var score = 0;
-var scoreText;
+var bricks;
+var gametime = 0;
 
 function preload() {
     game.load.image('logo', 'phaser.png');
     game.load.image('sky', 'assets/sky.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
+    game.load.image('brick_orange', 'assets/brick_orange_blank.png');
+    game.load.image('brick_orange_highlight', 'assets/brick_orange_highlight.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 function create() {
 
-    //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //  A simple background for our game
     game.add.sprite(0, 0, 'sky');
-    //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
-    //  We will enable physics for any object that is created in this group
     platforms.enableBody = true;
-    // Here we create the ground.
     var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     ground.scale.setTo(2, 2);
-    //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
-
-    //  Now let's create two ledges
     var ledge = platforms.create(400, 400, 'ground');
     ledge.body.immovable = true;
     ledge = platforms.create(-150, 250, 'ground');
     ledge.body.immovable = true;
 
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    bricks = game.add.group(game, game, true, true);
+    bricks.enableBody = true;
 
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
-
-    stars = game.add.group();
-
-    stars.enableBody = true;
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  Let gravity do its thing
-        star.body.gravity.y = 6;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    for (var i = 0; i < 30; ++i) {
+        var brick = Brick(Math.floor(Math.random() * 800), 0,
+                Math.floor(Math.random() * 20));
     }
 
-    scoreText = game.add.text(16, 16, 'score: 0',
-            {fontSize: '32px', fill: '#000'});
 }
 
+function Brick(x, y, number) {
+    var brick = bricks.create(x, y, 'brick_orange');
+    bricks.add(brick);
+    brick.number = number
+    brick.anchor.set(.5);
+    brick.scale.setTo(.5, .5);
+    brick.body.gravity.y = 20 + brick.number;
+    brick.body.bounce.x = 0.5;
+    brick.body.bounce.y = 0.2;
+
+    // brick number text:
+    var dx = 0;
+    if (brick.number >= 10) {
+        dx = -15;
+    }
+    var text = game.add.text(dx - brick.width / 4, -brick.height / 2 - 3,
+            brick.number, {font: "42px Arial", fill: "#ffffff"});
+    brick.addChild(text);
+
+    // brick mouse event:
+    brick.highlighted = false;
+    brick.inputEnabled = true;
+    brick.events.onInputDown.add(clickBrick, this);
+    return brick;
+}
+
+function clickBrick(brick) {
+    console.log('clicked a brick!')
+    if (brick.highlighted) {
+        brick.loadTexture('brick_orange');
+        brick.highlighted = false;
+    }
+    else {
+        brick.loadTexture('brick_orange_highlight');
+        brick.highlighted = true;
+    }
+}
 
 
 function update() {
-    //console.log("update yo");
-    //  
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
-
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
-
-
-    //  Reset the players velocity / movement:
-    player.body.velocity.x = 0;
-    if (cursors.left.isDown)
-    {
-        player.body.velocity.x = -150;
-        player.animations.play('left');
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.velocity.x = 150;
-        player.animations.play('right');
-    }
-    else
-    {
-        player.animations.stop();
-        player.frame = 4;
-    }
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -333;
-    }
-
-}
-
-function collectStar(player, star) {
-
-    // Removes the star from the screen
-    star.kill();
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
+    ++gametime;
+    
+    game.physics.arcade.collide(bricks, platforms);
+    game.physics.arcade.collide(bricks, bricks);
 
 }
