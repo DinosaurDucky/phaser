98db6f37b9ab25c9b8b8bb8194fd1b9f8d773ddc
DinosaurDucky <bhatch@ucsc.edu>
2015-11-08 17:32:02 -0800

    cleaned up code / re-ordered functions to something resembling a topological sort.
    next up: going to re-attempt column collapsing.
    
    TODO:
    
    * add/sub/mult/div
    	* UI buttons
    * more interesting scoring system
    * non-random number generation
    * columns collapse when empty

--- Numbo/public_html/Numbo.js
+++ Numbo/public_html/Numbo.js
@@ -64,69 +64,6 @@ function create() {
     game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(hitSpace);
 }
 
-
-function hitSpace() {
-    console.log("spacebar! clearing bottom.");
-    for (b in lit_bricks) {
-        lit_bricks[b].loadTexture('brick_orange');
-        lit_bricks[b].highlighted = false;
-    }
-    lit_bricks.splice(0, lit_bricks.length); // empty lit_bricks
-    bottom_bricks.destroy(true, true);
-}
-
-function brickMaker() {
-    Brick(-1, -1, -1);
-    game.time.events.add(Phaser.Timer.SECOND * SPAWN_TIMER, brickMaker);
-    //collapseColumns();
-}
-
-function sinkBrick(brick, row) {
-    game.add.tween(brick).to({y: rowToY(row)}, 100,
-            Phaser.Easing.Linear.In, true);
-}
-
-function sinkAll() {
-    for (var col = 0; col < brick_grid.length; ++col) {
-        sinkColumn(col);
-    }
-}
-function sinkColumn(col) {
-    for (var row = 0; row < brick_grid[col].length; ++row) {
-        sinkBrick(brick_grid[col][row], row);
-    }
-}
-
-function collapseColumnRight(col) {
-
-    brick_grid.splice(col, 1);
-    brick_grid.unshift([]);
-    for (var row = 0; row < brick_grid[col].length; ++row) {
-        game.add.tween(brick_grid[col][row]).to({x: colToX(col)}, 100,
-                Phaser.Easing.Linear.In, true);
-    }
-}
-
-"hi. this is a messaged for mike mcCawley, or whomever is now in charge of\n\
-partial IGETC.\n\
-my name is brett hatch, and my student id is 1439073. \n\
-i would like to make an appointment ASAP to discuss my options for fulfilling \n\
-my last igetc GE requirement.\n\
-i can be reached at bhatch@ucsc.edu or 831-246-4617. \n\
-thank you."
-
-function collapseColumns() {
-    var middle_col = Math.floor(NUM_COLUMNS / 2);
-    for (var col = NUM_COLUMNS - 1; col >= 0; --col) {
-        if (brick_grid[col].length == 0) {
-            collapseColumnRight(col);
-        }
-    }
-    for (var col = middle_col + 1; col < NUM_COLUMNS; ++col) {
-
-    }
-}
-
 // negative params for defaults. x default: random column. y default: 0.
 // number default: random int in {1, 2, ..., MAX_RANDOM}
 function Brick(col, row, number) {
@@ -172,6 +109,57 @@ function Brick(col, row, number) {
 
 }
 
+function brickMaker() {
+    Brick(-1, -1, -1);
+    game.time.events.add(Phaser.Timer.SECOND * SPAWN_TIMER, brickMaker);
+    //collapseColumns();
+}
+
+function sinkBrick(brick, row) {
+    game.add.tween(brick).to({y: rowToY(row)}, 100,
+            Phaser.Easing.Linear.In, true);
+}
+function sinkColumn(col) {
+    for (var row = 0; row < brick_grid[col].length; ++row) {
+        sinkBrick(brick_grid[col][row], row);
+    }
+}
+function sinkAll() {
+    for (var col = 0; col < brick_grid.length; ++col) {
+        sinkColumn(col);
+    }
+}
+
+function clickBrick(brick) {
+    if (lit_bricks.length == 0 ||
+            areAdjacent(brick, lit_bricks[lit_bricks.length - 1])) {
+        if (!brick.hightlighted) {
+            brick.loadTexture('brick_orange_highlight');
+            brick.highlighted = true;
+            lit_bricks.push(brick);
+            copyToBottom(brick);
+            if (lit_bricks.length >= 3)
+                checkSum(brick);
+
+        }
+    }
+}
+function areAdjacent(b1, b2) {
+    if (b1 == b2)
+        return false;
+
+    var c1 = getCol(b1);
+    var c2 = getCol(b2);
+    if (Math.abs(c1 - c2) > 1)
+        return false;
+
+    var r1 = getRow(b1);
+    var r2 = getRow(b2);
+    if (Math.abs(r1 - r2) > 1)
+        return false;
+
+    return true;
+}
 function copyToBottom(brick) {
     var index = lit_bricks.length;
     var bottomBrick = bottom_bricks.create(index * COLUMN_WIDTH / 2, 500, 'brick_orange');
@@ -188,15 +176,27 @@ function copyToBottom(brick) {
 
     return bottomBrick;
 }
+function checkSum(brick) {
+    if (lit_bricks.length == 1)
+        return;
 
-function findTopOfColumn(col) {
-    if (col < 0 || col >= NUM_COLUMNS)
-        return -2; // flag: invalid column
-    if (brick_grid[col].length < NUM_ROWS)
-        return brick_grid[col].length;
-    return -1; // flag: too many bricks already in this column (it's full)
+    var sum = -2 * brick.number;
+    for (b in lit_bricks)
+        sum += lit_bricks[b].number;
+
+    if (sum == 0) {
+        score += lit_bricks.length;
+        scoreText.text = 'Score: ' + score;
+        for (b in lit_bricks) {
+            removeBrickById(lit_bricks[b].id);
+            lit_bricks[b].kill();
+        }
+        lit_bricks.splice(0, lit_bricks.length); // empty lit_bricks
+        bottom_bricks.destroy(true, true);
+        sinkAll();
+        //collapseColumns();
+    }
 }
-
 function removeBrickById(id) {
     for (var col = 0; col < NUM_COLUMNS; ++col) {
         for (var row = 0; row < brick_grid[col].length; ++row) {
@@ -206,6 +206,45 @@ function removeBrickById(id) {
             }
         }
     }
+}
+
+function hitSpace() {
+    console.log("spacebar! clearing bottom.");
+    for (b in lit_bricks) {
+        lit_bricks[b].loadTexture('brick_orange');
+        lit_bricks[b].highlighted = false;
+    }
+    lit_bricks.splice(0, lit_bricks.length); // empty lit_bricks
+    bottom_bricks.destroy(true, true);
+}
+
+function collapseColumns() {
+    var middle_col = Math.floor(NUM_COLUMNS / 2);
+    for (var col = NUM_COLUMNS - 1; col >= 0; --col) {
+        if (brick_grid[col].length == 0) {
+            collapseColumnRight(col);
+        }
+    }
+    for (var col = middle_col + 1; col < NUM_COLUMNS; ++col) {
+
+    }
+}
+function collapseColumnRight(col) {
+
+    brick_grid.splice(col, 1);
+    brick_grid.unshift([]);
+    for (var row = 0; row < brick_grid[col].length; ++row) {
+        game.add.tween(brick_grid[col][row]).to({x: colToX(col)}, 100,
+                Phaser.Easing.Linear.In, true);
+    }
+}
+
+function findTopOfColumn(col) {
+    if (col < 0 || col >= NUM_COLUMNS)
+        return -2; // flag: invalid column
+    if (brick_grid[col].length < NUM_ROWS)
+        return brick_grid[col].length;
+    return -1; // flag: too many bricks already in this column (it's full)
 }
 
 function colToX(col) {
@@ -228,61 +267,6 @@ function getRow(brick) {
         }
     }
 }
-
-function areAdjacent(b1, b2) {
-    if (b1 == b2)
-        return false;
-
-    var c1 = getCol(b1);
-    var c2 = getCol(b2);
-    if (Math.abs(c1 - c2) > 1)
-        return false;
-
-    var r1 = getRow(b1);
-    var r2 = getRow(b2);
-    if (Math.abs(r1 - r2) > 1)
-        return false;
-
-    return true;
-}
-
-function clickBrick(brick) {
-    if (lit_bricks.length == 0 ||
-            areAdjacent(brick, lit_bricks[lit_bricks.length - 1])) {
-        if (!brick.hightlighted) {
-            brick.loadTexture('brick_orange_highlight');
-            brick.highlighted = true;
-            lit_bricks.push(brick);
-            copyToBottom(brick);
-            if (lit_bricks.length >= 3)
-                checkSum(brick);
-
-        }
-    }
-}
-
-function checkSum(brick) {
-    if (lit_bricks.length == 1)
-        return;
-
-    var sum = -2 * brick.number;
-    for (b in lit_bricks)
-        sum += lit_bricks[b].number;
-
-    if (sum == 0) {
-        score += lit_bricks.length;
-        scoreText.text = 'Score: ' + score;
-        for (b in lit_bricks) {
-            removeBrickById(lit_bricks[b].id);
-            lit_bricks[b].kill();
-        }
-        lit_bricks.splice(0, lit_bricks.length); // empty lit_bricks
-        bottom_bricks.destroy(true, true);
-        sinkAll();
-        //collapseColumns();
-    }
-}
-
 
 function update() {
     ++GAME_TIME;
